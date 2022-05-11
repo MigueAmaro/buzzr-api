f61cb066c5fa01a7e8940014af2969e63ecb9d86
Jdvd01 <velasquezdenorajose@gmail.com>
2022-05-11 22:45:43 +0000

    [FIX] port and db name

--- src/main.py
+++ src/main.py
@@ -87,5 +87,5 @@ def sign_up():
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
